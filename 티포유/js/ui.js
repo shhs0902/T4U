0a16698914fa2b5fb8da16714f385dd5d627828b
shhs0902 <70187019+shhs0902@users.noreply.github.com>
2022-04-03 16:10:53 +0900

    티포유 220403

--- 티포유/js/ui.js
+++ 티포유/js/ui.js
@@ -1,5 +1,9 @@
 (function() {
 
+  // 텍스트 변수
+  const ONSTRING = "on";
+  const REMSTRING = "rem";
+
   // function headerFixHandler() {
   //   const header = document.querySelector("header");
 
@@ -15,21 +19,25 @@
 
   
   const nav = document.querySelector("nav");
+
   // 햄버거 버튼(전체 페이지 공통)
   function hbgBtnEvHandler(){
     const hbgBtn = document.querySelector(".btn-hbg");
 
     hbgBtn.addEventListener("click", function(){
-        nav.classList.add("on");
+        nav.classList.add(ONSTRING);
     });
   };
 
   // gnb 메뉴 닫기(전체 페이지 공통)
   function gnbMenuClose(){
     const closeBtn = document.querySelector(".btn-close");
+    if(!closeBtn) {
+      return;
+    }
 
     closeBtn.addEventListener("click", function(){
-      nav.classList.remove("on");
+      nav.classList.remove(ONSTRING);
   });
   }
 
@@ -55,8 +63,8 @@
 
       const container = document.querySelector("#cont-02 .container");
 
-      conRig.style.width = (winW - (conLefW + conPoLeft + 17)) * 0.1 + "rem";
-      container.style.height = (slide.getBoundingClientRect().height) * 0.1 + "rem";
+      conRig.style.width = (winW - (conLefW + conPoLeft + 17)) * 0.1 + REMSTRING;
+      container.style.height = (slide.getBoundingClientRect().height) * 0.1 + REMSTRING;
     });
   }
 
@@ -64,6 +72,9 @@
   function tabEventHandler() {
     const tabBtn = document.querySelectorAll(".tab-btn");
     const tabContent = document.querySelectorAll(".tab-cont");
+    if(!tabBtn) {
+      return;
+    }
 
     [].forEach.call(tabBtn, function(btn, idx){
 
@@ -74,21 +85,17 @@
         const b = a.replace("#", "");
 
         [].forEach.call(tabBtn, function(item, x){
-          item.classList.remove("on");
-          tabContent[x].classList.remove("on");
+          item.classList.remove(ONSTRING);
+          tabContent[x].classList.remove(ONSTRING);
 
         });
-        document.getElementById(b).classList.add("on");
+        document.getElementById(b).classList.add(ONSTRING);
 
-        this.classList.add("on");
+        this.classList.add(ONSTRING);
         
       });
-
-      
     });
   }
-  tabEventHandler();
-  
 
   window.addEventListener("resize", function () {
     slideContainerWidthCalc();
@@ -98,5 +105,6 @@
     hbgBtnEvHandler();
     gnbMenuClose();
     slideContainerWidthCalc();
+    tabEventHandler();
   });
-}());
+}(window));
